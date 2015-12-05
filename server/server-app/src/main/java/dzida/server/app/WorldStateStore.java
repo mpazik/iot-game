e61c2dd83cc65f3d1e3abcab3abbcb69decbb612
Marek Pazik <poczta@marekpazik.pl>
2015-12-05 14:08:14 +0100

    DL: Loading map from file which reduced map loading time from 500ms to 50ms.

--- server/server-app/src/main/java/dzida/server/app/WorldStateStore.java
+++ server/server-app/src/main/java/dzida/server/app/WorldStateStore.java
@@ -1,23 +1,46 @@
 package dzida.server.app;
 
+import com.google.gson.Gson;
+import com.google.gson.stream.JsonReader;
 import dzida.server.core.position.model.Position;
 import dzida.server.core.world.model.WorldState;
 
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.net.URI;
 
 public class WorldStateStore {
     private static final URI staticServerAddress = URI.create("http://localhost:8080");
+    private static final String mapDirLocation = "assets/maps/";
 
     public WorldState loadMap(String name) {
-        WorldMap worldMap = getMapClient(name)
-                .request(MediaType.APPLICATION_JSON_TYPE)
-                .get(WorldMap.class);
+        long startTime = System.currentTimeMillis();
+        WorldMap worldMap = loadMapFromFile(name);
+        long mapLoadTime = System.currentTimeMillis() - startTime;
+        System.out.printf("Map %s loaded in %dms \n", name, mapLoadTime);
 
         Position spawnPoint = Position.of(worldMap.spawnPoint.x, worldMap.spawnPoint.y);
         return new WorldState(name, worldMap.width, worldMap.height, worldMap.tileset, spawnPoint, worldMap.tiles);
+    }
+
+    private WorldMap loadMapFromServer(String name) {
+        return getMapClient(name)
+                    .request(MediaType.APPLICATION_JSON_TYPE)
+                    .get(WorldMap.class);
+    }
+
+    private WorldMap loadMapFromFile(String name) {
+        String filePath = mapDirLocation + name + ".json";
+        try {
+            JsonReader reader = new JsonReader(new FileReader(filePath));
+            return new Gson().fromJson(reader, WorldMap.class);
+        } catch (FileNotFoundException e) {
+            System.out.println(e);
+            return null;
+        }
     }
 
     private WebTarget getMapClient(String name) {
