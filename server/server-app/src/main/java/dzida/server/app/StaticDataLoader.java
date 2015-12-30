b6374761e76e475dd7408adecf1d667640f8d183
Marek Pazik <poczta@marekpazik.pl>
2015-12-30 10:19:56 +0100

    #3: Reading zip connect by server.

--- server/server-app/src/main/java/dzida/server/app/StaticDataLoader.java
+++ server/server-app/src/main/java/dzida/server/app/StaticDataLoader.java
@@ -9,6 +9,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.net.URLConnection;
+import java.util.zip.GZIPInputStream;
 
 public class StaticDataLoader {
     public <T> T loadJsonFromServer(String path, TypeToken<T> typeToken) {
@@ -16,8 +18,10 @@ public class StaticDataLoader {
         try {
             URL url = UriBuilder.fromUri(Configuration.getStaticServerAddress()).path(path).build().toURL();
             System.out.printf("Downloading json file from %s \n", url);
-            InputStream inputStream = url.openStream();
-            JsonReader jsonReader = new JsonReader(new InputStreamReader(inputStream));
+
+            URLConnection urlConnection = url.openConnection();
+            String contentEncoding = urlConnection.getContentEncoding();
+            JsonReader jsonReader = new JsonReader(requestInputStream(url.openStream(), contentEncoding));
             T data = new Gson().fromJson(jsonReader, typeToken.getType());
 
             long loadTime = System.currentTimeMillis() - startTime;
@@ -28,4 +32,17 @@ public class StaticDataLoader {
             throw new RuntimeException(e);
         }
     }
+
+    private InputStreamReader requestInputStream(InputStream inputStream, String contentEncoding) {
+        if ("gzip".equals(contentEncoding)) {
+            try {
+                return new InputStreamReader(new GZIPInputStream(inputStream));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return new InputStreamReader(inputStream);
+    }
+
+
 }
