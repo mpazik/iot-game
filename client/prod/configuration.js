6c594418c20cbdc44a4360bdf6baf426d9a2cec6
Marek Pazik <poczta@marekpazik.pl>
2016-06-04 15:16:28 +0200

    Updated client configuration.

--- client/prod/configuration.js
+++ client/prod/configuration.js
@@ -3,7 +3,7 @@ Configuration = {
     tileSize: 48,
     spawnTime: 4000,
     devMode: true,
-    serverAddress: "ws://server.dzida-online.pl:7001",
-    containerRestAddress: "http://server.dzida-online.pl:7000",
-    assetsLocalization: "http://assets.dzida-online.pl"
+    serverAddress: "ws://ec2-52-59-245-12.eu-central-1.compute.amazonaws.com.:7001",
+    containerRestAddress: "http://ec2-52-59-245-12.eu-central-1.compute.amazonaws.com:7000",
+    assetsLocalization: "http://assets.dzida-online.pl.s3-website.eu-central-1.amazonaws.com"
 };
